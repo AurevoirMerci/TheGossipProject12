aed13907bbe4951ebb1d700151913ce17132b97e
geoffrey03 <geoffrey.poirrier@essec.edu>
2019-02-06 10:33:42 +0100

    essai heroku

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -3,7 +3,7 @@ class Gossip < ApplicationRecord
   has_many :gossip_tags
   has_many :tags , through: :gossip_tags
   validates :title,
-    presence: true, length: { minimum: 3 }, length: { maximum: 14 }
+    presence: true, length: { minimum: 3 , maximum: 14 }
   validates :content,
     presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'user/:id', to: 'user#show' , as: 'users'
   #get 'gossip/show'
   #get 'welcome/show'
-  get 'gossip/:id', to: 'gossip#show' , as: 'gossips'
+  #get 'gossip/:id', to: 'gossip#show' , as: 'gossips'
   get 'static_pages/team'
   get 'welcome/:user_entry', to: 'welcome#show'
   get 'static_pages/team'
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/static_pages/contact', to: 'controller#method'
+
+  resources :gossip
 end
